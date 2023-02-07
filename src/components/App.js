f4467a270a6a00f56d9156abb071b0a543ef5253
Parmjeet Kaur <work.technologies25@gmail.com>
2023-02-07 18:35:02 +1000

    done1

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,13 @@ function Home() {
     </div>
   );
 }
+function About(){
+  return(
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  )
+}
 
 {/* write an <About> component here */}
 
@@ -23,8 +30,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <About/>
     </div>
   );
 }
